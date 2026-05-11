e166c74148211454fc889e54947ffb92218177f9
Tom Karlo <tkarlo@Toms-Mac-Studio-65.local>
2026-05-11 19:31:05 +0100

    Update base URL for demo-cycling-dashboard repo name

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/camden-cycling-dashboard/',
+  base: '/demo-cycling-dashboard/',
   plugins: [react(), tailwindcss()],
 })
